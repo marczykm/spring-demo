7f2e80b628e18a236750cd0a5ea76e5e48ee94d1
kenji89 <mruczek89@gmail.com>
2015-06-30 22:40:09 +0200

    replaced @Configuration, @EnableAutoConfiguration and @ComponentScan annotation with one @SpringBootApplication

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 @RestController
 public class Application {
 
