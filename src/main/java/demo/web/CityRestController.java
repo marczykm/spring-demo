7aa7edeb19a9ebb7cacfba193a41b096410fbf4e
kenji89 <mruczek89@gmail.com>
2015-07-05 09:45:22 +0200

    Revert "removed other sites, leaved one as a starter for angular page"
    This reverts commit 901d3b06a2e39fd9fd8f7018fae827c4d8a23b5c.

--- src/main/java/demo/web/CityRestController.java
+++ src/main/java/demo/web/CityRestController.java
@@ -19,12 +19,12 @@ public class CityRestController {
     private CityService cityService;
 
     @RequestMapping("/city")
-    public List<City> citiesAll(){
+    public List<City> findAll(){
         return cityService.findAll();
     }
 
     @RequestMapping("/city/{name}/{state}")
-    public City cityDetails(@PathVariable("name") String name, @PathVariable("state") String state){
+    public City city(@PathVariable("name") String name, @PathVariable("state") String state){
         return cityService.findOneByNameAndStateIgnoreCase(name, state);
     }
 }
--- src/main/java/demo/web/SiteController.java
+++ src/main/java/demo/web/SiteController.java
@@ -1,6 +1,13 @@
 package demo.web;
 
+import demo.domain.City;
+import demo.domain.CityRepository;
+import demo.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by MMARCZYK on 2015-07-02.
@@ -8,7 +15,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class SiteController {
 
-    public String index() {
-        return "index";
+    @Autowired
+    private CityService cityService;
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/city/{name}/{state}")
+    public String city(@PathVariable("name") String name, @PathVariable("state") String state, Model model){
+        City city = cityService.findOneByNameAndStateIgnoreCase(name, state);
+
+        if (city != null){
+            model.addAttribute("city", city);
+        }
+        return "city";
     }
 }
